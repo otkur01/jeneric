4b2bdd84c5a685af67392a721d372e93a2e88c03
otkur <abdulhekem@gmail.com>
2024-02-16 12:12:36 +0300

    Given a string, write a method to find the first non-repeating vowel

---
+++ dayspractice/javaProgamming/src/codingbat/firstNotRepitingVowel.java
@@ -0,0 +1,34 @@
+package codingbat;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class firstNotRepitingVowel {
+    public static int firstNonRepeatingVowel(String str) {
+        str = str.toLowerCase();
+        Map<Character, Integer> charMap = new LinkedHashMap<>();
+        for (char ch : str.toCharArray()) {
+            charMap.put(ch, charMap.getOrDefault(ch, 0) + 1);
+        }
+
+        String temp = "";
+        for (Character eachcha : charMap.keySet()) {
+            if ((eachcha == 'a' || eachcha == 'e' || eachcha == 'i' || eachcha == 'u' || eachcha == 'o') && charMap.get(eachcha) == 1) {
+
+                temp = "" + eachcha;
+                break;
+            }
+
+        }
+        if (str.contains(temp) && !temp.isEmpty()) return str.indexOf(temp);
+        else return -1;
+    }
+
+    public static void main(String[] args) {
+
+
+            String str = "moodeellli";
+            System.out.println(firstNonRepeatingVowel(str));
+        }
+    }
