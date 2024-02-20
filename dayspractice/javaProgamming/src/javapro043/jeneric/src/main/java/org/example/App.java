d008714031dff3a1e5b859760e0102f2d9f0c6ac
otkur <abdulhekem@gmail.com>
2024-02-20 09:55:51 +0300

    creat new generic ,adding idem to arraylist

---
+++ dayspractice/javaProgamming/src/javapro043/jeneric/src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ArrayList item = new ArrayList();
+        item.add(1);
+        item.add(2);
+        item.add(3);
+        item.add(4);
+
+        printDuonble(item);
+
+
+
+
+        System.out.println( "Hello World!" );
+    }
+
+    private static void printDuonble(ArrayList item) {
+        for (Object o : item) {
+            System.out.println((Integer)o);
+        }
+    }
+}
