1bf0278512ce40626c3f21244f51da5dcbcf6f54
otkur <abdulhekem@gmail.com>
2024-02-21 13:02:58 +0300

    testing swap method with string array

--- dayspractice/javaProgamming/src/javapro043/jeneric/generinTask/src/main/java/org/example/SwapValue.java
+++ dayspractice/javaProgamming/src/javapro043/jeneric/generinTask/src/main/java/org/example/SwapValue.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class SwapValue {
     public static void main(String[] args) {
         Integer []a = {1,2,3,4,5,6};
         Swap_value(a,2,4);
         System.out.println(Arrays.toString(a));
-
+        String [] str = {"alim","hekim","salim","kamil","salih", "azad"};
+        Swap_value(str,1,3);
+        System.out.println(Arrays.toString(str));
     }
     public static <T> void Swap_value(T[] ts, int a, int b){
         T temp = ts[a];
