8bd4b308c888fff088628c9645d34eb9755f706d
otkur <abdulhekem@gmail.com>
2024-02-21 11:07:07 +0300

    some generic problem tasks

--- dayspractice/javaProgamming/src/javapro043/jeneric/src/main/java/org/example/Main.java
+++ dayspractice/javaProgamming/src/javapro043/jeneric/src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+       FootballPlayer joe = new FootballPlayer("joe");
+       BasablePlayer leo = new BasablePlayer("leo");
+       SoccerPlayer keo = new SoccerPlayer("keo");
+
+       Team<SoccerPlayer> liver = new Team("liver");
+       //liver.addPlayer(joe);
+       //liver.addPlayer(leo);
+       liver.addPlayer(keo);
+       Team<BasablePlayer> brokenTeam = new Team<>("rong");
+
 
     }
 }
--- dayspractice/javaProgamming/src/javapro043/jeneric/src/main/java/org/example/Team.java
+++ dayspractice/javaProgamming/src/javapro043/jeneric/src/main/java/org/example/Team.java
@@ -3,10 +3,10 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team<T> {
+public class Team<T extends Player> {
     private String name;
 
-    private List<Player> members = new ArrayList<>();
+    private List<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -15,14 +15,14 @@ public class Team<T> {
     public String getName() {
         return name;
     }
-    public boolean addPlayer(Player player){
+    public boolean addPlayer(T player){
         if(members.contains(player)){
-            System.out.println(player.getName() + " is already on the team");
+            System.out.println(((Player)player).getName() + " is already on the team");
             return false;
         }
         else {
             members.add(player);
-            System.out.println(player.getName()+ " picked for team  " + this.name);
+            System.out.println(((Player)player).getName()+ " picked for team  " + this.name);
             return true;
         }
     }
